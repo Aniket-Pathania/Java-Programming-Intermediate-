8a5c6d9d7d296a6fd41ecc8c43b796ce0a33457b
Aniket-Pathania <aniketpathania313@gmail.com>
2026-07-10 19:59:32 +0530

    Add files via upload

---
+++ Problem-Solving/ReverseWordsInAString.java
@@ -0,0 +1,36 @@
+
+public class ReverseWordsInAString {
+    static String reverseWords(String s){
+        StringBuilder sb = new StringBuilder();
+        int i = s.length()-1;
+        while(i>=0){
+            while(i>=0 && s.charAt(i)== ' '){
+                i--;
+            }
+            if (i<0){
+                break;
+            }
+            int j =i;
+            while(j>=0 && s.charAt(j)!=' '){
+                j--;
+            }
+            sb.append(s.substring(j+1,i+1));
+            while(j>=0 && s.charAt(j)==' '){
+                j--;
+            }
+            if (j>=0){
+                sb.append(' ');
+            }
+            i=j;
+        }
+        return sb.toString();
+    }
+    public static void main(String[] args){
+
+        String s = "  the   sky   is blue  ";
+
+        String ans = reverseWords(s);
+
+        System.out.println(ans);
+    }
+}
