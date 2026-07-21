be0e77f650593fc8ff03d8854a957cbd77877bd3
Aniket-Pathania <aniketpathania313@gmail.com>
2026-07-21 23:36:58 +0530

    Add files via upload

---
+++ Problem-Solving/RemoveAllAdjacentDuplicatesInAString.java
@@ -0,0 +1,24 @@
+    public class RemoveAllAdjacentDuplicatesInAString {
+        public static String removeDuplicates(String s){
+            StringBuilder ans = new StringBuilder();
+            for (int i=0;i<s.length();i++){
+                char ch = s.charAt(i);
+                if (ans.length()>0 && ans.charAt(ans.length()-1)==ch){
+                    ans.deleteCharAt(ans.length()-1);
+                }
+                else{
+                    ans.append(ch);
+                }
+
+            }
+            return ans.toString();
+        }
+        public static void main(String[] args){
+
+            String s = "abbaca";
+
+            String ans = removeDuplicates(s);
+
+            System.out.println("Final String = " + ans);
+        }
+    }
